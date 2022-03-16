e02ae1c6155be125dde53c1f72dc865af6633f20
Célia Figueiredo <celianlfg@hotmail.com>
2022-03-16 11:30:30 +0000

    delete import matplotlib

--- transformData.py
+++ transformData.py
@@ -2,9 +2,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
-import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 
